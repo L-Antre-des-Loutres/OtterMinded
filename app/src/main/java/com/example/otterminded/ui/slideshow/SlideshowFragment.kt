137d074453397a99bad44841881f5a093ab6a368
matheo-prd <perod.matheo@gmail.com>
2024-03-28 10:14:30 +0100

    Maj CreateQuestionActivity

--- app/src/main/java/com/example/otterminded/ui/slideshow/SlideshowFragment.kt
+++ app/src/main/java/com/example/otterminded/ui/slideshow/SlideshowFragment.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
