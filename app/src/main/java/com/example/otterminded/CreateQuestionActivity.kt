f72b600982fa06480586439d0fbf43c7999a37dc
Victor Buil <victorbuil11000@gmail.com>
2024-03-28 11:02:13 +0100

    adaptation du l'activiter et sa vue

--- app/src/main/java/com/example/otterminded/CreateQuestionActivity.kt
+++ app/src/main/java/com/example/otterminded/CreateQuestionActivity.kt
@@ -5,13 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Spinner
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
 
 class CreateQuestionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_create_question)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        val textView: TextView = findViewById(R.id.textView2)
+        val spinner: Spinner = findViewById(R.id.spinner)
+        val questionEditText: TextInputEditText = findViewById(R.id.question)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.constraintLayout)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
